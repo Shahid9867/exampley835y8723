6b569c52e07ab34f82ac1aaf5d8e8c23e27462d8
Shahid9867 <91383021+Shahid9867@users.noreply.github.com>
2022-12-01 13:06:28 +0530

    Add files via upload

---
+++ SELF_PRACTICE/practice.js
@@ -0,0 +1,18 @@
+document.getElementById("testing")
+document.getElementsByClassName("classtest")
+document.getElementsByTagName("div")
+
+
+document.querySelector("#testing")
+document.querySelector(".classtest")
+document.querySelector("div")
+
+
+document.querySelectorAll("#testing")
+document.querySelectorAll(".classtest")
+document.querySelectorAll("div")
+
+// document.getElementById("testing").innerText = "Text has changed";
+// document.getElementById("testing").innerText = "<h1>Only the text has changed</h1>";
+// // document.getElementById("testing").innerHTML = "<h1>Html elment also has changed along with the text</h1>";
+document.getElementById("testing").style.color = "red"
